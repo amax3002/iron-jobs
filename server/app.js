70ff73f6fc0966d8feeab4c75c7c65c6c9bd48ff
amax3002 <alexandremguy@gmil.com>
2016-12-15 13:49:48 -0500

    setup

--- server/app.js
+++ server/app.js
@@ -1,2 +1,16 @@
+var express = require('express');
+
+var server = express();
 
 console.log('Hello World');
+
+
+server.set('port', process.env.PORT || 3000);
+
+server.listen(server.get('port'), function serverStarted(err) {
+  if(err) {
+      console.error(err);
+  } else {
+    console.log('Server started at http://localhost:' + server.get('port'));
+  }
+});
---
+++ server/routes/jobs.js
@@ -0,0 +1,5 @@
+var mongodb = require('mongodb');
+
+var express= require('express');
+
+var router = express.Router();
