01b9dbbc7aab99c5e604570ff9c21f727e360cb9
amax3002 <alexandremguy@gmil.com>
2016-12-15 16:54:17 -0500

    end of day

--- server/models/jobs.js
+++ server/models/jobs.js
@@ -1,10 +1,11 @@
 var dbConnect = require('./db-connect');
+var ObjectID = require('mongodb').ObjectID;
 
 module.exports = {
   getAll,
-  // getOne,
-  create
-  // destroy
+  getOne,
+  create,
+  destroy
 };
 
 
@@ -16,9 +17,40 @@ function getAll(done) {
       }
       db.collection('jobs')
         .find()
-        .toArray(done);
+        .toArray( function mapData(err, data) {
+          var mappedData = data.map(function (data) {
+            return {
+              'id': data._id,
+              'company': data.company,
+              'link': data.link
+            };
+          });
+          done(err, mappedData);
+        });
     });
 }
+
+
+function getOne(id, done) {
+  dbConnect(function connectHandler(err, db) {
+    if(err) {
+        done(err, null);
+        return;
+    }
+    db.collection('jobs')
+      .findOne({_id: new ObjectID(id)}, function callback(err, data) {
+        cleanData = {
+          'id': data._id,
+          'company': data.company,
+          'notes': data.notes,
+          'link': data.link,
+          'createTime': data.createTime
+        }
+        done(null, cleanData);
+      });
+  });
+}
+
 
 function create(data, done) {
     dbConnect(function connectHandler(err, db) {
@@ -32,3 +64,16 @@ function create(data, done) {
             .insert(data, done);
     });
 }
+
+function destroy(data, done) {
+    dbConnect(function connectHandler(err, db) {
+        if (err) {
+          done(err, null);
+          return;
+        }
+        db.collection('jobs')
+          .findOne({_id: new ObjectID(id)}, function callback(err, data) {
+            done(null, data);
+          });
+      });
+    }
--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -4,6 +4,7 @@ var jobsModel = require('../models/jobs');
 var router = express.Router();
 
 router.get('/', function allJobs(req, res) {
+
   jobsModel.getAll(function dataRetrieved(err, data) {
     if(err) {
       console.log(err);
@@ -15,8 +16,15 @@ router.get('/', function allJobs(req, res) {
 });
 
 
-router.get('/:id([a-f0-9]{24})', function getAllJobs(req, res) {
-    res.json({ id: req.params.id, company: req.params.company, link: req.params.link });
+router.get('/:id([a-f0-9]{24})', function getAJob(req, res) {
+  jobsModel.getOne(req.param.id, function dataRetriever(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Uh oh...couldn\'t get your data');
+      return;
+    }
+    res.json(data);
+  });
 });
 
 
@@ -29,9 +37,22 @@ router.post('/', function createJob(req, res) {
           res.status(500).send('Uh oh...couldn\'t get your data');
           return;
         }
-        res.json( data );
+        res.json( {
+            'company': data.ops[0].company,
+            'notes': data.ops[0].notes,
+            'link': data.ops[0].link,
+            'createTime': data.ops[0].createTime
+        });
     });
 });
 
+router.delete('/:id', function removeJob(req, res) {
+  if (err) {
+    console.log(err);
+    res.status(500).send('Uh oh...couldn\'t get your data');
+    return;
+  }
+
+})
 
 module.exports = router;
